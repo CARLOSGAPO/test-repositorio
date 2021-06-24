77d079e4ac10a38435b1a5c82926315f246d6af4
CARLOSGAPO <carlosmasr122@gmail.com>
2021-06-24 11:51:07 -0500

    s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import country from './country';
+
 
 function App() {
   return (
     <div className="App">
-      <country/>
+      
     </div>
   );
 }
