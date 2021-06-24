a4183077e866edbab071b44bb579a5f11ce3a072
CARLOSGAPO <carlosmasr122@gmail.com>
2021-06-24 11:44:08 -0500

    s

--- src/country.jsx
+++ src/country.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Styledcountry = styled.div`
-    
-`;
+const Styledcountry = styled.div``;
 
 function country () {
     return (
