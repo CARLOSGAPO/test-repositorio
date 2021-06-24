406c5bf5dc007421e315406f1e4e83e08c7ab2c2
CARLOSGAPO <carlosmasr122@gmail.com>
2021-06-24 11:59:51 -0500

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import country from './country'
+
 function App() {
   return (
     <div className="App">
-     <country/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
