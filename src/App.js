3fa1b79a8c3c9f4c0bd4b09f65a99ca4ca5f09f3
CARLOSGAPO <carlosmasr122@gmail.com>
2021-06-24 11:48:26 -0500

    s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import country from './country';
 function App() {
   return (
     <div className="App">
-      <country> </country>
+      <country/>
     </div>
   );
 }
---
+++ src/country.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styledcountry = styled.div``;
+
+function country () {
+    return (
+        <Styledcountry>
+         Country   
+        </Styledcountry>
+    )
+};
+
+export default country;
